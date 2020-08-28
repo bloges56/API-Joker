b75920cf76d518d65b9e35775f951e2f9e39a20b
Brady Logan <brady.drew.logan@gmail.com>
2020-08-28 14:20:40 -0500

    adjusted JokeProvider main.js and the index so that the user can now select what type of joke they want

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -4,11 +4,12 @@ let joke;
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
 
-export const getJoke = () => {
-    return fetch("https://official-joke-api.appspot.com/jokes/random")
+export const getJoke = (type) => {
+    const urlString =  `https://official-joke-api.appspot.com/jokes/${type}/random`;
+    return fetch(urlString)
         .then(response => response.json())
         .then((parsedJoke) => {
-            joke = parsedJoke
+            joke = parsedJoke[0]
         })
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,8 @@ const jokeCont = document.querySelector(".joke");
 jokeBtn.addEventListener("click", (e) => {
   console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
-  getJoke()
+  const type = document.querySelector("#types").value;
+  getJoke(type)
     .then( _ => {
       return useJoke()
     })
