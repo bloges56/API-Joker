ac5f71389558efbe068124a4d43a2b3684958cb9
Brady Logan <brady.drew.logan@gmail.com>
2020-08-28 10:49:21 -0500

    JokeProvider now sets joke variable to fetched object and returns it

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -4,6 +4,16 @@ let joke;
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
 export const useJoke = () => {
+    var retObject;
+    return Object.assign(retObject, joke);
+}
+
+
+export const getJoke = () => {
     return fetch("https://official-joke-api.appspot.com/jokes/random")
         .then(response => response.json())
+        .then((response) => {
+            joke = response.json()
+        })
 }
+
