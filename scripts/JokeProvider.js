60741544905e3110ee4c9ebca8e5bc9b5491ac52
Brady Logan <brady.drew.logan@gmail.com>
2020-08-28 11:03:50 -0500

    working JokeProvider

--- scripts/JokeProvider.js
+++ scripts/JokeProvider.js
@@ -4,16 +4,18 @@ let joke;
 //         Advanced: find a way to return a copy of the joke object (tip: .slice will not work)
 // 2. define and export a getJoke function that fetches a joke from the API and sets the joke variable to the response
 export const useJoke = () => {
-    var retObject;
-    return Object.assign(retObject, joke);
+    debugger;
+    // var retObject;
+    // return Object.assign(retObject, joke);
+    return joke;
 }
 
 
 export const getJoke = () => {
     return fetch("https://official-joke-api.appspot.com/jokes/random")
         .then(response => response.json())
-        .then((response) => {
-            joke = response.json()
+        .then((parsedJoke) => {
+            joke = parsedJoke
         })
 }
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
+import { getJoke, useJoke } from './JokeProvider.js'
+
 const jokeBtn = document.querySelector(".request-joke-btn");
 
 jokeBtn.addEventListener("click", (e) => {
   console.log("I should be grabbing a joke now...");
   // TODO: fetch a joke from the Joke API and render it to the DOM
 });
+
+
+getJoke();
+var joke = useJoke();
